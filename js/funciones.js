f82cff3827ea8bb4ee562bfd4da7bd95af10fad6
Erick Camacho <camachorosaserick@gmail.com>
2022-08-18 15:58:32 -0500

    agregando mi primer proyecto web

---
+++ js/funciones.js
@@ -0,0 +1,35 @@
+jQuery(document).ready(listo);
+
+
+
+function listo()
+{
+    jQuery(".hamb").click(function(e){
+        e.preventDefault();
+        jQuery("header .container nav").toggleClass("open");
+        
+        jQuery(".hamb i").toggleClass("fa-xmark");
+    });
+
+    jQuery("header .container nav a").click(function(){
+       
+        jQuery("header .container nav").removeClass("open");
+        jQuery(".hamb i").removeClass("fa-xmark");
+        var dev = jQuery(this).attr("href");
+
+        jQuery("html,body").animate({
+            "scrollTop": jQuery(dev).offset().top -76
+        })  
+
+    })
+
+    jQuery("main .bloque-inicio a").click(function(){
+
+        var nos = jQuery(this).attr("href");
+
+        jQuery("html,body").animate({
+            "scrollTop": jQuery(nos).offset().top -76
+
+    })
+})
+}
